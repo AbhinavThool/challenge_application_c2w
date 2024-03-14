91e66304cdec22c69edfdc45847a60b79812ba4f
Abhinav Thool <abhinavthool@gmail.com>
2024-03-14 14:39:02 +0530

    added svg

---
+++ lib/Screens/interests_page_one.dart
@@ -0,0 +1,35 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
+
+// class InterestsScreenOne extends StatefulWidget {
+//   const InterestsScreenOne({super.key});
+
+//   @override
+//   State<InterestsScreenOne> createState() => _InterestsScreenOneState();
+// }
+
+// class _InterestsScreenOneState extends State<InterestsScreenOne> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Please Select your Interests"),
+//       ),
+//       body: ListView.builder(
+//         itemCount: 10,
+//         itemBuilder: (context, index) {
+//           Container(
+//             height: 100,
+//             width: double.infinity,
+//             child: Row(
+//               children: [
+//                 Icon(Icons.live_tv_outlined),
+//                 Text("Personal Growth"),
+//               Checkbox(value: value, onChanged: onChanged)],
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
